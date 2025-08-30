f33a886a63dea0ca1308ce271d9fda50428eaa1d
Nicole Erler <n.s.erler@umcutrecht.nl>
2025-08-30 16:42:11 +0200

    first commit

---
+++ R/hello.R
@@ -0,0 +1,11 @@
+#' Title
+#' Hello World Function
+#' @returns Nothing, just prints "Hello, world!" to the console.
+#' @export
+#'
+#' @examples
+#' hello()
+#'
+hello <- function() {
+  print("Hello, world!")
+}
